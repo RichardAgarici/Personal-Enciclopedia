d88d9ebe8f9a412c8e4f3fe239237a404ff76cd3
Richard Agarici <laurentiu.agarici@gmail.com>
2023-04-26 21:40:06 +0200

    adding a few lines to demonstrate the logic behind set

--- HashSet/src/hashset/v1/SetMain.java
+++ HashSet/src/hashset/v1/SetMain.java
@@ -1,6 +1,4 @@
 package hashset.v1;
-
-import java.lang.reflect.Array;
 import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
@@ -37,11 +35,48 @@ public class SetMain {
 
         Set<String> nature = new HashSet<>();
         Set<String> divine = new HashSet<>();
-        String[] arrayNatureWords = {"all","nature","is","but","art","unknown","to","thee"};
+        String[] arrayNatureWords = {"all", "nature", "is", "but", "art", "unknown", "to", "thee"};
         nature.addAll(Arrays.asList(arrayNatureWords));
 
-        String[] divineWords = {"to","err","is","human","to","forgive","divine"};
+        String[] divineWords = {"to", "err", "is", "human", "to", "forgive", "divine"};
         divine.addAll(Arrays.asList(divineWords));
 
+        System.out.println("\nnature - divine");
+        Set<String> diff1 = new HashSet<>(nature);
+        diff1.removeAll(divine);
+        printSet(diff1);
+
+        System.out.println("\ndivine - nature");
+        Set<String> diff2 = new HashSet<>(divine);
+        diff2.removeAll(nature);
+
+        printSet(diff2);
+
+        Set<String>unionTest=new HashSet<>(nature);
+        unionTest.addAll(divine);
+        Set<String>intersectionTest= new HashSet<>(nature);
+        intersectionTest.retainAll(divine);
+
+        System.out.println("Symmetric difference");
+        unionTest.removeAll(intersectionTest);
+        printSet(unionTest);
+
+        if(nature.containsAll(divine)){
+            System.out.println("divine is a subset of nature");
+        }
+        if (nature.containsAll(intersectionTest)){
+            System.out.println("intersection is a subset of nature");
+        }
+        if (divine.containsAll(intersectionTest)) {
+            System.out.println("intersection is a subset of divine ");
+        }
     }
+        private static void printSet (Set<String> set) {
+            System.out.print("\t");
+            for (String s : set) {
+                System.out.print(s+" ");
+            }
+        }
+
+
 }
