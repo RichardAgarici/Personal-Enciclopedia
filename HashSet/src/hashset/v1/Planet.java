c3c39bc484476609e5cf06eb13f8d69dd31d73ab
Richard Agarici <laurentiu.agarici@gmail.com>
2023-04-27 13:35:26 +0200

    updateing final version

--- HashSet/src/hashset/v1/Planet.java
+++ HashSet/src/hashset/v1/Planet.java
@@ -1,17 +1,15 @@
 package hashset.v1;
 
 public class Planet extends HeavenlyBody {
-
     public Planet(String name, double orbitalPeriod) {
         super(name, orbitalPeriod, BodyTypes.PLANET);
     }
 
     @Override
     public boolean addSatellite(HeavenlyBody moon) {
-        if(moon.getBodyType() ==BodyTypes.MOON){
+        if(moon.getKey().getBodyType() == BodyTypes.MOON) {
             return super.addSatellite(moon);
-        }
-        else {
+        } else {
             return false;
         }
     }
