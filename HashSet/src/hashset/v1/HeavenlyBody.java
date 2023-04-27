b8843e1063bc2ddf335191ed3756d644fb6d6fa3
Richard Agarici <laurentiu.agarici@gmail.com>
2023-04-27 12:34:19 +0200

    finishing a few settings

--- HashSet/src/hashset/v1/HeavenlyBody.java
+++ HashSet/src/hashset/v1/HeavenlyBody.java
@@ -3,15 +3,27 @@ package hashset.v1;
 import java.util.HashSet;
 import java.util.Set;
 
-public final class HeavenlyBody {
+public class HeavenlyBody {
     private final String name;
     private final double orbitalPeriod;
     private final Set<HeavenlyBody> satellites;
+    private final BodyTypes bodyType;
 
-    public HeavenlyBody(String name, double orbitalPeriod) {
+    public enum BodyTypes {
+        STAR,
+        PLANET,
+        DWARF_PLANET,
+        MOON,
+        COMET,
+        ASTEROID
+    }
+
+    public HeavenlyBody(String name, double orbitalPeriod, BodyTypes bodyType) {
         this.name = name;
         this.orbitalPeriod = orbitalPeriod;
         this.satellites = new HashSet<>();
+        this.bodyType=bodyType;
+
     }
 
     public String getName() {
@@ -22,8 +34,12 @@ public final class HeavenlyBody {
         return orbitalPeriod;
     }
 
-    public boolean addMoon(HeavenlyBody moon) {
-        return this.satellites.add(moon);
+    public BodyTypes getBodyType() {
+        return bodyType;
+    }
+
+    public boolean addSatellite(HeavenlyBody moon) {
+            return this.satellites.add(moon);
     }
 
     public Set<HeavenlyBody> getSatellites() {
@@ -31,24 +47,26 @@ public final class HeavenlyBody {
     }
 
     @Override
-    public boolean equals(Object obj) {
+    public final boolean equals(Object obj) {
         if (this == obj) {
             return true;
         }
-
-        System.out.println("obj.getClass() is " + obj.getClass());
-        System.out.println("this.getClass() is " + this.getClass());
-        if ((obj == null) || (obj.getClass() != this.getClass())) {
-            return false;
+        if(obj instanceof HeavenlyBody){
+            HeavenlyBody theObject = (HeavenlyBody) obj;
+            if(this.name.equals(theObject.getName())){
+                return this.bodyType == theObject.getBodyType();
+            }
         }
-
-        String objName = ((HeavenlyBody) obj).getName();
-        return this.name.equals(objName);
+      return false;
     }
 
     @Override
-    public int hashCode() {
-        System.out.println("hashcode called");
-        return this.name.hashCode()+57;
+    public final int hashCode() {
+        return this.name.hashCode()+57+this.bodyType.hashCode();
+    }
+
+    @Override
+    public String toString() {
+        return this.name+": "+this.bodyType+", "+this.orbitalPeriod;
     }
 }
---
+++ HashSet/src/hashset/v1/Moon.java
@@ -0,0 +1,7 @@
+package hashset.v1;
+
+public class Moon extends HeavenlyBody{
+    public Moon(String name, double orbitalPeriod) {
+        super(name, orbitalPeriod, BodyTypes.MOON);
+    }
+}
